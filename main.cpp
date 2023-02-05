b770973fd15d6ea4a32f5bcfb1451987eff89cd7
juliannea <112399053+juliannea@users.noreply.github.com>
2023-02-05 14:30:53 -0500

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,21 @@
+/*
+Spring 2023 - Lab 02
+Last name: Aguilar
+First name: Julianne
+GitHub username: juliannea
+*/
+#include <iostream>
+#include "funcs.h"
+
+int main()
+{
+    int L;
+    int U;
+
+    std::cout<<"Enter L\n";
+    std::cin>>L;
+    std::cout<<"Enter U\n";
+    std::cin>>U;
+
+    print_interval(L, U);
+}
