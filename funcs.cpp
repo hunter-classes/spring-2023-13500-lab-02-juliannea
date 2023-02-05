11f728e0a641f9956769f65e191b2fb39f46f6fe
juliannea <112399053+juliannea@users.noreply.github.com>
2023-02-05 14:31:46 -0500

    Create funcs.cpp

---
+++ funcs.cpp
@@ -0,0 +1,19 @@
+*
+Spring 2023 - Lab 02
+Last name: Aguilar
+First name: Julianne
+GitHub username: juliannea
+*/
+
+#include <iostream>
+#include "funcs.h"
+
+
+
+void print_interval(int L, int U)
+{
+    for(int i = L; i < U; i++)
+    {
+        std::cout<<i<<" ";
+    }
+}
